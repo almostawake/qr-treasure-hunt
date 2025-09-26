2eef9e2fc86aceecaf92a3428cc6a269aa553aab
Andrew Walker <andrew@almostawake.com>
2025-09-26 21:16:24 +0200

    Fixed login, works in emulator mode.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# template
+## Install & run locally
+
+`npm run install:all`
+
+`npm run start:emulators` (in one terminal)
+
+`npm run start:client` (in another terminal)
+
+## Set up new environment
+
+## Build & deploy
+
+Build before deployment to ensure no compilation errors across the board.
+
+`npm run build`
+
+`npm run deploy`
--- client/src/hooks/UserService.ts
+++ client/src/hooks/UserService.ts
@@ -24,26 +24,17 @@ export class UserService {
   async login(): Promise<void> {
     const { auth } = await getFirebaseServices()
     const googleProvider = new GoogleAuthProvider()
-    try {
-      await signInWithPopup(auth, googleProvider)
-    } catch (error) {
-      console.error('Login error:', error)
-      throw error
-    }
+    await signInWithPopup(auth, googleProvider)
   }
 
   async logout(): Promise<void> {
     const { auth } = await getFirebaseServices()
-    try {
-      await signOut(auth)
-    } catch (error) {
-      console.error('Logout error:', error)
-      throw error
-    }
+    await signOut(auth)
   }
 
   async initialize(): Promise<() => void> {
     const { auth } = await getFirebaseServices()
+
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       this.setUser(user)
       this.setLoading(false)
